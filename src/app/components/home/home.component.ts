74441f94ede5416289b5607f02c47a4f85d9148a
Ironspider25 <yashmp2573@gmail.com>
2024-02-09 00:07:34 +0530

    tailwind installation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { SwiperComponent } from '../swiper/swiper.component';
+import { ProductListingComponent } from '../product-listing/product-listing.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, SwiperComponent],
+  imports: [HeaderComponent, FooterComponent, SwiperComponent, ProductListingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
