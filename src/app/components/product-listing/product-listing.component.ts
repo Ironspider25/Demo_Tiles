2440026286762e7b9da0d1839c73ed76c64ad5ee
Ironspider25 <yashmp2573@gmail.com>
2024-02-11 12:43:39 +0530

    adding loader

--- src/app/components/product-listing/product-listing.component.ts
+++ src/app/components/product-listing/product-listing.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-product-listing',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './product-listing.component.html',
   styleUrl: './product-listing.component.scss'
 })
